5eae8c5d27aea6137a2de7e16f64f8f737834fe1
mattluurocks <mattluu@gmail.com>
2021-09-16 15:51:12 +0800

    Update wallet.tsx

--- js/packages/common/src/contexts/wallet.tsx
+++ js/packages/common/src/contexts/wallet.tsx
@@ -66,7 +66,7 @@ export const WalletModal: FC = () => {
           marginBottom: 10,
         }}
       >
-        TWDA
+        TW
       </div>
 
       <h2>{selected ? 'Change provider' : 'Welcome to TW Digital Art'}</h2>
